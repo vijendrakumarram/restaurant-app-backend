71639b2121533d80e346f444420588b9de84aa86
vijendrakumarram <your_email@example.com>
2025-07-14 18:16:41 +0530

    decode userid

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -10,7 +10,8 @@ module.exports = (req, res, next) => {
   const token = authHeader.split(' ')[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded.id;
+    //req.userId = decoded.id;
+    req.userId = decoded.userId; // ✅ important
     next();
   } catch (err) {
     return res.status(401).json({ error: 'Invalid or expired token' });
