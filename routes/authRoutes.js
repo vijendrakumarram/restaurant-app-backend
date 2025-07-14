6cd041d909f4317172d8fd63cdfa26f296ebf725
vijendrakumarram <your_email@example.com>
2025-07-14 13:42:16 +0530

    Add login route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const { register } = require('../controllers/authController');
 
 router.post('/register', register);
+router.post('/login', login); 
 
 module.exports = router;
